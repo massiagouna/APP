4fd660c7fb2f5eefbb19a84711d67354f4df6031
massiagouna <104739521+massiagouna@users.noreply.github.com>
2022-07-20 17:22:35 +0200

    Update MAIN3.py

--- MAIN3.py
+++ MAIN3.py
@@ -21,7 +21,7 @@ classifier_name = st.sidebar.selectbox(
     ('RandomForestRegressor', 'LGBMRegressor', 'XGBRegressor')
 )
 
-modellgbm = pkl.load(open(r"C:\Users\HP\Downloads\projet_geek\geek\model.pkl", 'rb') )
+modellgbm = pkl.load(https://github.com/massiagouna/APP/blob/main/model.pkl) )
 
 #col1, col2, col3, col4, col5 = st.columns(5)
 fo = st.text_input('date_evaluation')
